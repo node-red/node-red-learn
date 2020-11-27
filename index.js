a17233c7c867713a6c8b05e92ed7503751d3a251
Nick O'Leary <knolleary@users.noreply.github.com>
2020-11-27 13:33:53 +0000

    Add some comments

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const EventEmitter = require('events');
 const express = require("express");
 const http = require("http");
-const REDClientAPI = require("@node-red/editor-api");
+
+// Load the Node-RED editor components
+const REDEditorAPI = require("@node-red/editor-api");
 const REDUtil = require("@node-red/util");
 
-const app = express();
+// Load our fake runtime
+const runtimeAPI = require("./lib/runtime/index.js");
 
 const PORT = process.env.PORT || 8000;
 
-const runtimeAPI = require("./lib/runtime/index.js");
-
+// Create the base app and http server
+const app = express();
 const server = http.createServer(function(req,res) { app(req,res);});
 server.on('error', function(err) {
     console.log(err);
 });
 
+// Setup the runtime settings
 const settings = {
     httpNodeRoot: "/",
     version: "1.2.4",
@@ -23,18 +27,22 @@ const settings = {
         header: { title: " Node-RED Learn"},
         palette: { editable: false }
     }
-
-
 };
+
+// Initialise our fake runtime
 runtimeAPI.init({
     events: new EventEmitter(),
     settings:settings
 });
-REDUtil.init(settings);
-REDClientAPI.init(settings,server,{},runtimeAPI);
-REDClientAPI.start().then(() => {
-    app.use(REDClientAPI.httpAdmin);
 
+// Initialise the Node-RED components
+REDUtil.init(settings);
+REDEditorAPI.init(settings,server,{},runtimeAPI);
+
+// Start the editor-api
+REDEditorAPI.start().then(() => {
+    // Attach the editor API app to ours
+    app.use("/", REDEditorAPI.httpAdmin);
     server.listen(PORT,function() {
         console.log(`http://localhost:${PORT}`);
     });
