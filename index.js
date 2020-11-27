bb4a135f5b27108e2cfbf526cff718da1a5ddb7e
Nick O'Leary <knolleary@users.noreply.github.com>
2020-11-27 16:39:02 +0000

    Load the core nodes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const http = require("http");
 // Load the Node-RED editor components
 const REDEditorAPI = require("@node-red/editor-api");
 const REDUtil = require("@node-red/util");
+const REDRegistry = require("@node-red/registry");
 
 // Load our fake runtime
 const runtimeAPI = require("./lib/runtime/index.js");
@@ -20,27 +21,40 @@ server.on('error', function(err) {
 
 // Setup the runtime settings
 const settings = {
+    available: ()  => false,
     httpNodeRoot: "/",
     version: "1.2.4",
     editorTheme: {
         page: { title: "Node-RED Learn"},
         header: { title: " Node-RED Learn"},
         palette: { editable: false }
-    }
+    },
+    coreNodesDir: __dirname+"/node_modules/@node-red/nodes",
 };
 
-// Initialise our fake runtime
-runtimeAPI.init({
+// Initialise our fake runtime - this seems to be the minimal set needed
+// to get the core nodes loaded.
+// There may still be some bits of function needed.
+const runtime = {
     events: new EventEmitter(),
-    settings:settings
-});
+    settings:settings,
+    log: REDUtil.log,
+    util: REDUtil.util,
+    nodes: { registerType: () => {}},
+    library: { register: () => {}},
+    get adminApp() { return REDEditorAPI.httpAdmin },
+}
+
+runtimeAPI.init(runtime);
 
 // Initialise the Node-RED components
 REDUtil.init(settings);
 REDEditorAPI.init(settings,server,{},runtimeAPI);
+REDRegistry.init(runtime)
 
+
+REDRegistry.load().then(() => REDEditorAPI.start()).then(() => {
 // Start the editor-api
-REDEditorAPI.start().then(() => {
     // Attach the editor API app to ours
     app.use("/", REDEditorAPI.httpAdmin);
     server.listen(PORT,function() {
